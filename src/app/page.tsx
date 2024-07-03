62496b3fc2a24cb2c89f1977730df3ca33482f9b
Tyson Do <doraeminemon@gmail.com>
2024-07-03 14:39:32 +0700

    Finish website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { MdAdd, MdPrint, MdMicrowave, MdSchedule, MdChevronRight } from 'react-icons/md'
 
 export default function Home() {
   return (
@@ -21,10 +22,12 @@ export default function Home() {
       </div>
       <div className="main-body grid lg:grid-cols-2 lg:px-16 px-8 lg:py-12 py-6 gap-8">
         <div className="col-1 order-2 lg:order-1">
-          <div className="breadcrumb uppercase text-xs flex font-bold pb-4">
-            <div className="item pr-1">recipes</div>
-            <div className="item pr-1">bread</div>
-            <div className="item pr-1">quick bread</div>
+          <div className="breadcrumb uppercase text-xs flex font-bold pb-4 items-center">
+            <div className="item">recipes</div>
+            <MdChevronRight size={16}/>
+            <div className="item">bread</div>
+            <MdChevronRight size={16}/>
+            <div className="item">quick bread</div>
           </div>
           <div className="title text-2xl pb-14">Whole-Grain Banana Bread</div>
           <div className="description pb-4">
@@ -32,6 +35,9 @@ export default function Home() {
 While the recipe calls for a 50/50 mix of flours (all-purpose and whole wheat), we often make the bread 100% whole wheat, and honestly? No one can tell, it&apos;s that good! And not only is this bread delicious — it&apos;s versatile.
           </div>
           <div className="time flex pb-4 gap-4">
+            <div className="item flex items-center">
+              <MdSchedule size={30}/>
+            </div>
             <div className="item">
               <div className="step uppercase text-xs">prep</div>
               <div className="time">10 mins</div>
@@ -45,13 +51,28 @@ While the recipe calls for a 50/50 mix of flours (all-purpose and whole wheat), 
               <div className="time">1 hr 10 mins</div>
             </div>
           </div>
-          <div className="more-items flex gap-4">
+          <div className="more-items flex gap-4 flex-wrap">
+            <div className="item flex items-center">
+              <MdMicrowave size={30} />
+            </div>
             <div className="item">
               <div className="step uppercase text-xs">yield</div>
               <div className="time">1 loaf, 12 generous servings</div>
             </div>
-            <button className="button uppercase border border-red-500 rounded-md px-3 text-sm">Save recipe</button>
-            <button className="button uppercase border border-red-500 rounded-md px-3 text-sm">Print</button>
+            <div className="buttons flex gap-2 flex-grow justify-between lg:justify-start">
+              <button className="button uppercase border border-red-500 rounded-md px-3 text-xs lg:text-sm flex gap-2 items-center py-2 w-1/2">
+                <MdAdd className="lg:text-[20px]"/>
+                <div className="flex justify-center flex-grow">
+                  Save recipe
+                </div>
+              </button>
+              <button className="button uppercase border border-red-500 rounded-md px-3 text-xs lg:text-sm flex gap-2 items-center py-2 w-1/2">
+                <MdPrint className="lg:text-[20px]"/>
+                <div className="flex justify-center flex-grow">
+                  Print
+                </div>
+              </button>
+            </div>
           </div>
         </div>
         <div className="col-2 order-1 lg:order-2">
